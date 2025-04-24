de8d3b90930e9884dc78e107f3059dc2f86ba86b
Yu Chung Lee <yuchung.lee@wsu.edu>
2025-04-23 18:36:56 -0700

    added additional routes to get info for front page

--- client/src/SearchComponent.jsx
+++ client/src/SearchComponent.jsx
@@ -7,73 +7,56 @@ export default function SearchComponent() {
   const [results, setResults] = useState([]);
   const [chartData, setChartData] = useState([]);
 
-  const handleSearch = () => {
-    if (searchType === "athlete" && query.toLowerCase() === "usain bolt") {
-      setResults(["Usain Bolt"]);
-      setChartData([
-        { event: "100m", medals: 3 },
-        { event: "200m", medals: 3 },
-        { event: "4x100m Relay", medals: 2 },
-      ]);
-    } else if (searchType === "event" && query.toLowerCase() === "men's basketball") {
-      setResults(["Men's Basketball"]);
-      setChartData([
-        { team: "USA", golds: 15 },
-        { team: "Soviet Union", golds: 2 },
-        { team: "Yugoslavia", golds: 1 },
-        { team: "Argentina", golds: 1 },
-      ]);
-    } else if (searchType === "year" && query === "2004") {
-      setResults(["2004 Olympics"]);
-      setChartData([
-        { country: "USA", medals: 101 },
-        { country: "China", medals: 63 },
-        { country: "Russia", medals: 90 },
-        { country: "Australia", medals: 49 },
-        { country: "Germany", medals: 41 },
-      ]);
-    } else {
-      setResults([]);
-      setChartData([]);
-    }
-  };
-
-  const createQuery = (searchType, query) => {
-    let dbQuery = ''
-    if (searchType === 'athelete') {
-      dbQuery = 'SELECT Sport, Event, SUM(Medal) FROM Athlete, Event, Athlete_Event WHERE Athlete.ID = Athlete_Event.ID AND Event.E_Id = Athlete_Event.E_Id AND LOWER(Name) = ' + query.toLowerCase() + ' GROUP BY Sport, Event'
-    } else if (searchType === 'event') {
-      dbQuery = 'SELECT Team, SUM(Medal) FROM Athlete, Event, Athlete_Event, Team, Athlete_Team WHERE Athlete.ID = Athlete_Event.ID AND Event.E_Id = Athlete_Event.E_Id AND Athlete.ID = Athelete_Team.ID AND Athlete_Team.T_Id = Team.T_Id AND LOWER(Event) = ' + query.toLowerCase() + ' GROUP BY Team'
-    } else if (searchType === 'year') {
-      dbQuery = 'SELECT Region, SUM(Medal) FROM Athlete, Event, Athlete_Event, Region, Athlete_Region WHERE Athlete.ID = Athlete_Event.ID AND Event.E_Id = Athlete_Event.E_Id AND Athlete.ID = Athlete_Region.ID AND Athlete_Region.NOC = Region.NOC AND LOWER(Year) = ' + query.toLowerCase() + ' GROUP BY Region'
-    }
-    return dbQuery
-  }
-
   // const handleSearch = () => {
-  //   const [query, setQuery] = useState("");
-  //   const [searchType, setSearchType] = useState("athlete");
-  //   const [results, setResults] = useState([]);
-  //   const [chartData, setChartData] = useState([]);
-  //   const dbQuery = createQuery(searchType, query);
-
-  //   // send dbQuery
-
-  //   // if no results
-  //   setResults([]);
-  //   setChartData([]);
-  //   //else
-  //   if (searchType === 'athelete') {
-  //     setResults([query])
-  //     setChartData([]) // input dbQuery data
-  //   } else if (searchType === 'event') {
-  //     setResults([query])
-  //     setChartData([]) // input dbQuery data
-  //   } else if (searchType === 'year') {
-  //     setResults([query + ' Olympics'])
-  //     setChartData([]) // input dbQuery data
+  //   if (searchType === "athlete" && query.toLowerCase() === "usain bolt") {
+  //     setResults(["Usain Bolt"]);
+  //     setChartData([
+  //       { event: "100m", medals: 3 },
+  //       { event: "200m", medals: 3 },
+  //       { event: "4x100m Relay", medals: 2 },
+  //     ]);
+  //   } else if (searchType === "event" && query.toLowerCase() === "men's basketball") {
+  //     setResults(["Men's Basketball"]);
+  //     setChartData([
+  //       { team: "USA", golds: 15 },
+  //       { team: "Soviet Union", golds: 2 },
+  //       { team: "Yugoslavia", golds: 1 },
+  //       { team: "Argentina", golds: 1 },
+  //     ]);
+  //   } else if (searchType === "year" && query === "2004") {
+  //     setResults(["2004 Olympics"]);
+  //     setChartData([
+  //       { country: "USA", medals: 101 },
+  //       { country: "China", medals: 63 },
+  //       { country: "Russia", medals: 90 },
+  //       { country: "Australia", medals: 49 },
+  //       { country: "Germany", medals: 41 },
+  //     ]);
+  //   } else {
+  //     setResults([]);
+  //     setChartData([]);
   //   }
-  // }
+  // };
+
+  const handleSearch = async () => {
+    // if no results
+    setResults([]);
+    setChartData([]);
+    //else
+    if (searchType === 'athelete') {
+      const res = await fetch(`http://localhost:5000/api/getAthleteInfo/${query.toLowerCase()}`)
+      setResults([query])
+      setChartData([res]) // input dbQuery data
+    } else if (searchType === 'event') {
+      const res = await fetch(`http://localhost:5000/api/getEventInfo/${query.toLowerCase()}`)
+      setResults([query])
+      setChartData([res]) // input dbQuery data
+    } else if (searchType === 'year') {
+      const res = await fetch(`http://localhost:5000/api/getYearInfo/${query.toLowerCase()}`)
+      setResults([query + ' Olympics'])
+      setChartData([res]) // input dbQuery data
+    }
+  }
 
   return (
     <div>
